dbdf994d5444706e7e66c66a5a1e37455acccb6a
RaiderSoap <info@chivalrystudio.com>
2018-06-30 12:57:19 -0700

    Adding Airbnb coding style guide .

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,26 +1,26 @@
 import * as React from 'react';
 
 export interface Props {
-    name: string;
-    enthusiasmLevel?: number;
+  name: string;
+  enthusiasmLevel?: number;
 }
 
 class Hello extends React.Component<Props, object> {
-    render() {
-        const {name, enthusiasmLevel = 1} = this.props;
+  render() {
+    const { name, enthusiasmLevel = 1 } = this.props;
 
-        if (enthusiasmLevel <= 0) {
-            throw new Error('You could be a little more enthusiastic. :D');
-        }
-
-        return (
-            <div className="hello">
-                <div className="greeting">
-                    Hello {name + getExclamationMarks(enthusiasmLevel)}
-                </div>
-            </div>
-        );
+    if (enthusiasmLevel <= 0) {
+      throw new Error('You could be a little more enthusiastic. :D');
     }
+
+    return (
+      <div className="hello">
+        <div className="greeting">
+          Hello {name + getExclamationMarks(enthusiasmLevel)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Hello;
@@ -28,5 +28,5 @@ export default Hello;
 // helpers
 
 function getExclamationMarks(numChars: number) {
-    return Array(numChars + 1).join('!');
+  return Array(numChars + 1).join('!');
 }
