b4641741125e101df76d74524710165f47ea2d37
Seiji Emery <seijiemery@gmail.com>
2018-07-04 13:12:31 -0700

    finished initial work on pisa refactor (mongodb TBD...?)

--- crawlers/ucsc/ucsc/spiders/pisa.py
+++ crawlers/ucsc/ucsc/spiders/pisa.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
+import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from ucsc.items import PisaIndexItem, PisaCourseItem
-import re
 
 site_path = lambda path: '{}/{}'.format(
     'https://pisa.ucsc.edu/class_search', path)
@@ -13,6 +14,11 @@ class PisaSpider(scrapy.Spider):
     allowed_domains = ['pisa.ucsc.edu']
     search_url = site_path('index.php')
     start_urls = [ search_url ]
+
+    def __init__(self, *args, **kwargs):
+        logger = logging.getLogger('scrapy.spidermiddlewares.httperror')
+        logger.setLevel(logging.WARNING)
+        super(PisaSpider, self).__init__(*args, **kwargs)
 
     def parse(self, response):
         yield scrapy.FormRequest(url=self.search_url, 
@@ -50,10 +56,25 @@ class PisaSpider(scrapy.Spider):
             match = re.match(r'(\w+\s+\d+)\s+-\s+(\d+)[^\w]+([^\n]+)', title_info)
             if not match:
                 raise Exception("Failed to parse '%s'"%title_info)
-
             result['course_name'] = match.group(1)
             result['course_section'] = match.group(2)
             result['course_name'] = match.group(3)
+
+            # grab class number
+            result['class_number'] = item.xpath('//a[contains(@id,"class_nbr")]').extract()[0]
+
+            # TBD: grab everything else (is wrapped in a <div class="row">, so maybe
+            # use an xpath selector for that and iterate its div children...?
+            # Each remaining field is in text inside of one of these divs; use
+            # regexes to clean up and eg. separate class_type ("LEC") from location, etc...
+
+            # Also TBD: this is slow AF so maybe (eventually) optimize this to, well,
+            # have less shit performance (note: prob xpath), but also be properly async 
+            # (visit page => generate async todo-queue to process each item individually)
+            # so that we can use multiprocessing to further speed up
+
+            # TBD: also need to store the term info (retain this somehow?),
+            # and would ideally like a timestamp somewhere on the outputted data
 
             yield result
             #yield scrapy.Request(request['url'], callback=self.parse_course_page)
@@ -63,3 +84,5 @@ class PisaSpider(scrapy.Spider):
         result = PisaCourseItem()
         result['content'] = str(content.extract())
         yield result
+
+        # TBD: actually parse this and process it, etc...
