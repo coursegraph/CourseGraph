dfc088d2e654432a8e4d670c922b6748033fdc91
RaiderSoap <info@chivalrystudio.com>
2018-07-05 00:07:48 -0700

    Migrate to ES8

--- server/index.js
+++ server/index.js
@@ -1,33 +1,61 @@
 const express = require('express');
 const next = require('next');
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const PORT = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
+
 const app = next({dev});
-const handle = app.getRequestHandler();
+const defaultRequestHandler = app.getRequestHandler();
+
+const LOCAL_DB = 'test';
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/${LOCAL_DB}`;
 
 app.prepare()
     .then(() => {
         const server = express();
 
+        server.use(bodyParser.json());
+        server.use((req, res, next) => {
+            req.db = db;
+            next()
+        });
+
+        // MongoDB
+        mongoose.Promise = Promise;
+        mongoose.connect(MONGODB_URI);
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+
+        // Next.js request handling
+        const customRequestHandler = (page, req, res) => {
+            // Both query and params will be available in getInitialProps({query})
+            const mergedQuery = Object.assign({}, req.query, req.params);
+            app.render(req, res, page, mergedQuery);
+        };
+
+        // Routes
         server.get('/a', (req, res) => {
-            return app.render(req, res, '/b', req.query);
+            return app.render(req, res, '/b', req.query)
         });
 
         server.get('/b', (req, res) => {
-            return app.render(req, res, '/a', req.query);
+            return app.render(req, res, '/a', req.query)
         });
 
         server.get('/posts/:id', (req, res) => {
-            return app.render(req, res, '/posts', {id: req.params.id});
+            return app.render(req, res, '/posts', {id: req.params.id})
         });
 
-        server.get('*', (req, res) => {
-            return handle(req, res);
-        });
+        server.get('/', customRequestHandler.bind(undefined, '/'));
+        server.get('*', defaultRequestHandler);
 
-        server.listen(port, (err) => {
-            if (err) throw err;
-            console.log(`> Ready on http://localhost:${port}`);
-        });
+        server.listen(PORT, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log(`> Ready on http://localhost:${PORT}`)
+        })
     });
