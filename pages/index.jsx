4c586ed09675bd05466b16961f9c43b2bae5f970
RaiderSoap <info@chivalrystudio.com>
2018-07-04 20:45:34 -0700

    Fix dev bug

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 export default () => (
     <div>
-        <style jsx>{`
-      p {
-        color: red;
-      }
-    `}</style>
         <p>Hello World!</p>
     </div>
 );
