b690088454d83ca2c885b43da1d861e8731cc55b
Wendy Liang <rliang4@ucsc.edu>
2018-07-13 14:53:04 -0700

    Popups

--- components/Popups.jsx
+++ components/Popups.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
-import Popup from "reactjs-popup";
+import Popup from 'reactjs-popup';
 
 class Popups extends React.Component {
-    render() {
-        return (
-            <div>
-                <Popup trigger={<a className="button"> Course name </a>} modal>
-                    {close => (
-                        <div className="modal">
-                            <a className="close" onClick={close}>
-                                &times;
-                            </a>
-                            <div className="header"> Course info</div>
-                            <div className="content">
-                                {" "}
-                                Course description
-                                <br/>
-                                Course prerequirements
-                            </div>
-                        </div>
-                    )}
-                </Popup>
+
+  static defaultProps = {
+    list: ({
+      course_id: 'CMPS115',
+      course_title: 'CMPS115 - Software Engineering',
+      course_description: 'asffodfi',
+      course_instructor: 'Richard',
+    }),
+  }
+
+  render() {
+    return (
+      <div>
+        <Popup trigger={<a className="button">{this.props.list.course_id}</a>} modal>
+          {close => (
+            <div className="modal">
+              <a className="close" onClick={close}>&times;</a>
+
+              <div className="header">{this.props.list.course_title}</div>
+              <div className="content">
+                <p>{'Course Description: '}{this.props.list.course_description}</p>
+                <p>{'Instructor: '}{this.props.list.course_instructor}</p>
+              </div>
             </div>
-        )
-    }
+          )}
+        </Popup>
+      </div>
+    );
+  }
 }
 
-export default Popups
+export default Popups;
