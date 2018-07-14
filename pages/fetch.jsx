abe12e9b7e510ce167a865f97836c073b2855fc1
RaiderSoap <info@chivalrystudio.com>
2018-07-14 10:15:47 -0700

    Adding proper List view

--- pages/fetch.jsx
+++ pages/fetch.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import fetch from 'isomorphic-unfetch';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
 class Index extends React.Component {
   static propTypes = {
     courses: PropTypes.arrayOf(PropTypes.shape({
@@ -24,16 +39,20 @@ class Index extends React.Component {
     return (
       <div>
         <h1>UCSC Courses</h1>
-        <ul>
-          {this.props.courses.map(({course_title}) => (
-            <li key={course_title}>
-              <a>{course_title}</a>
-            </li>
-          ))}
-        </ul>
+        <List>
+          {
+            this.props.courses.map(({course_title}) => (
+              <ListItem button component="a" href="https://www.ucsc.edu/">
+                <ListItemText key={course_title}
+                              primary={course_title}/>
+              </ListItem>
+            ))
+          }
+        </List>
+        <Divider/>
       </div>
     );
   }
 }
 
-export default Index;
+export default withStyles(styles)(Index);
