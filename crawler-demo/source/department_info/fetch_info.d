fd84c9a3dde1be0f6889c7661917649b086f22f7
Seiji Emery <seijiemery@gmail.com>
2018-07-14 19:48:07 -0700

    implemented childRange as a full bidirectional, random-access range

--- crawler-demo/source/department_info/fetch_info.d
+++ crawler-demo/source/department_info/fetch_info.d
@@ -1,7 +1,7 @@
 module department_info.fetch_info;
 import department_info.model;
 import util.fetch_html: fetchHtml;
-import util.search_utils: range, regexMatch;
+import util.search_utils: childRange, regexMatch;
 import std.stdio;
 import arsd.dom;
 
@@ -18,24 +18,27 @@ DepartmentInfo fetchInfo (DepartmentInfo dept) {
             .requireSelector("h1[id=title]")
             .innerText;
 
-        auto content = main
-            .requireSelector("div[class~=content]");
+        auto content = main.requireSelector("div[class~=content]")
+            .childRange
+            .requireSeq((child) { 
+                return child.tagName == "p" &&
+                    child.regexMatch!`(\d+\-\d+ General Catalog)`(dept.catalogVersion); 
+            })
+            .requireSeq((child) {
+                return child.tagName == "p" &&
+                    child.regexMatch!`([\s.]+)`(dept.departmentAddress);
+            })
+            .requireSeq((child) { 
+                return child.tagName == "hr";
+            });
 
-        content
-            .range
-            .requireSeq((elem) { 
-                return elem.tagName == "p" &&
-                    elem.regexMatch!`(\d+\-\d+ General Catalog)`(dept.catalogVersion); 
-            })
-            .requireSeq((elem) {
-                return elem.tagName == "p" &&
-                    elem.regexMatch!`([\s.]+)`(dept.departmentAddress);
-            })
-            .requireSeq((elem) { 
-                return elem.tagName == "hr";
-            })
-        ;
-        writefln("%s\n", content.innerText);
+        while (!content.empty) {
+            for (; !content.empty && content.front.tagName != "h1" && content.front.tagName != "h2"; content.popFront) {}
+            if (content.empty) break;
+
+            string section = content.moveFront.innerText;
+            writefln("Got section: '%s'", section);
+        }
     });
     return dept;
 }
--- crawler-demo/source/util/search_utils.d
+++ crawler-demo/source/util/search_utils.d
@@ -3,9 +3,10 @@ import arsd.dom;
 import std.regex;
 import std.exception: enforce;
 import std.format: format;
+import std.stdio: writefln;
 
-ElementRange range (Element elem) {
-    return ElementRange(elem);
+ElementRange childRange (Element elem) {
+    return ElementRange(elem, 0, elem.children.length);
 }
 
 bool regexMatch (string regex, Args...)(Element elem, ref Args args) {
@@ -19,22 +20,48 @@ bool regexMatch (string regex, Args...)(Element elem, ref Args args) {
 }
 
 struct ElementRange {
-    Element head; size_t child;
+    private Element head;
+    private size_t start, stop, current;
 
-    this (Element elem) { this.head = elem; child = 0; }
-    this (Element elem, size_t child) { this.head = elem; this.child = child; }
+    this (Element elem, size_t start, size_t stop) {
+        this.head = elem;
+        this.current = this.start = start;
+        this.stop = stop;
+    }
+    private this (Element elem, size_t start, size_t stop, size_t current) {
+        this.head = elem;
+        this.start = start;
+        this.stop = stop;
+        this.current = current;
+    }
 
-    @property Element front () { return head[child]; }
-    bool empty () { return head[child] !is null; }
-    void popFront () { ++child; }
-    ElementRange save () { return ElementRange(head, child); }
+    private bool bounded () { return current >= start && current < stop; }
+    @property Element front () { assert(bounded); return head[current]; }
+    bool empty () { return !bounded; }
+    void popFront () { assert(bounded); ++current; }
+    Element moveFront () { assert(bounded); return head[current++]; }
+    ElementRange save () { return ElementRange(head, start, stop, current); }
+    Element back () { assert(bounded); return head[stop - 1]; }
+    void popBack () { assert(bounded); --stop; }
+    size_t length () { return stop - start; }
+    Element opIndex (size_t i) { assert(start + i < length); return head[start + i]; }
 
     ref ElementRange requireSeq (bool delegate (Element elem) predicate) {
         auto start = save();
+        //writefln("Range empty? %s", empty);
         while (!empty && !predicate(front)) {
+            //writefln("Did not match '%s'", front);
             popFront;
         }
         enforce(!empty, format("Failed to find sequence, starting at %s", save.front.innerHTML));
+        //writefln("Matched! %s", front);
         return this;
+    }
+
+    unittest {
+        import std.range.primitives;
+        static assert(isForwardRange!ElementRange);
+        static assert(isBidirectionalRange!ElementRange);
+        static assert(isRandomAccessRange!ElementRange);
     }
 }
