e256b4b18c98f08bbe5884a052a84639abdb894d
Seiji Emery <seijiemery@gmail.com>
2018-07-15 01:24:05 -0700

    added jsonizer for output

--- crawler-demo/source/department_info/model.d
+++ crawler-demo/source/department_info/model.d
@@ -1,46 +1,78 @@
 module department_info.model;
 public import std.format: format;
-import std.string: toUpper, toLower;
+import std.string;
+import std.conv: to;
+import std.array;
+import std.algorithm;
+import jsonizer;
 
 
 class DepartmentInfo {
 public:
-    string departmentId;
-    string programUrl;
-    string coursesUrl;
-    string facultyUrl;
+    mixin JsonizeMe;
+
+    @jsonize {
+        string departmentId;
+        string programUrl;
+        string coursesUrl;
+        string facultyUrl;
+    }
+    
     Exception error = null;
 
-    string catalogVersion;
-    string departmentName;
-    string departmentUrl;
-    string departmentAddress;
-    string departmentPhoneNumber;
-    string lastCourseRevisionDate;
+    @jsonize {
+        string catalogVersion;
+        string departmentName;
+        string departmentUrl;
+        string departmentAddress;
+        string departmentPhoneNumber;
+        string lastCourseRevisionDate;
 
-    string rawProgramStatement;
+        string rawProgramStatement;
 
-    FacultyListing[string] faculty;
-    CourseListing[string] courses;
+        FacultyListing[string] faculty;
+        CourseListing[string] courses;
+    }
 
     struct ProgramListing {
-        string section;
-        string content;
+        mixin JsonizeMe;
+        @jsonize {
+            string section;
+            string content;
+        }
+        //string toString () { 
+        //    return format(`{ "section": "%s", "content": "%s" }`, 
+        //        section, content); 
+        //}
     }
     struct FacultyListing {
-        string name;
-        string title;
-        string department;
-        string description;
+        mixin JsonizeMe;
+        @jsonize {
+            string name;
+            string title;
+            string department;
+            string description;
+        }
+        //string toString () {
+        //    return format(`"%s": { "title": "%s", "dept": "%s", "description": "%s" }`,
+        //        name, title, department, description);
+        //}
     }
     struct CourseListing {
-        string name;
-        string title;
-        string division;
-        string terms;
-        string instructor;
-        string description;
-        string geCategories;
+        mixin JsonizeMe;
+        @jsonize {
+            string name;
+            string title;
+            string division;
+            string terms;
+            string instructor;
+            string description;
+            string geCategories;
+        }
+        //string toString () {
+        //    return format(`"%s": { "title": "%s", "division": "%s", "terms": "%s", "instructor": "%s", "description": "%s", "GE": "%s" }`,
+        //        name, title, division, terms, instructor, description, geCategories);
+        //}
     }
 
     this (string baseUrl, string departmentId) {
@@ -50,4 +82,29 @@ public:
         this.coursesUrl = format("%s/course-descriptions/%s.html", baseUrl, departmentId);
         this.facultyUrl = format("%s/faculty/%s.html", baseUrl, departmentId);
     }
+
+    //override string toString () {
+    //    return format(`"%s": {
+    //        "name": "%s",
+    //        "courses-revision": "%s", 
+    //        "etc": {
+    //            "program-page": "%s",
+    //            "courses-page": "%s",
+    //            "faculty-page": "%s",
+    //            "homepage": "%s",
+    //            "address": "%s",
+    //            "phone": "%s",
+    //        },
+    //        "faculty": {
+    //            %s
+    //        },
+    //        "courses": {
+    //            %s
+    //        }
+    //    }`, departmentId, departmentName, catalogVersion, programUrl, coursesUrl, facultyUrl,
+    //        departmentUrl, departmentAddress, departmentPhoneNumber,
+    //        faculty.values.map!"a.to!string".join(", "),
+    //        courses.values.map!"a.to!string".join(", ")
+    //    );
+    //}
 }
