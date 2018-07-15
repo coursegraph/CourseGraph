61e773d32fd5a773f624e19322ef4ca047d2cb61
Seiji Emery <seijiemery@gmail.com>
2018-07-14 20:55:19 -0700

    wrote simple expect() library and refactored unittests

--- crawler-demo/source/department_info/package.d
+++ crawler-demo/source/department_info/package.d
@@ -14,46 +14,44 @@ DepartmentInfo fetchDepartment (string baseUrl, string departmentId) {
 }
 
 unittest {
-    auto dept = fetchDepartment("https://registrar.ucsc.edu/catalog/archive/17-18/programs-courses", "math");
-    assert(dept.departmentId == "MATH", 
-        format("Expected department id = 'MATH', not '%s'", dept.departmentId));
-    assert(dept.programUrl == "https://registrar.ucsc.edu/catalog/archive/17-18/programs-courses/program-statements/math.html",
-        format("Expected program url = '...', not '%s'", dept.programUrl));
-    assert(dept.coursesUrl == "https://registrar.ucsc.edu/catalog/archive/17-18/programs-courses/course-descriptions/math.html",
-        format("Expected course url = '...', not '%s'", dept.coursesUrl));
-    assert(dept.facultyUrl == "https://registrar.ucsc.edu/catalog/archive/17-18/programs-courses/faculty/math.html",
-        format("Expected faculty url = '...', not '%s'", dept.facultyUrl));
- 
-    assert(!dept.error, format("Got exception:\n\t%s", dept.error));
-    assert(dept.catalogVersion == "2017-18 General Catalog");
-    assert(dept.departmentName == "Mathematics");
-    assert(dept.departmentUrl == "http://www.math.ucsc.edu");
-    assert(dept.departmentAddress == "4111 McHenry");
-    assert(dept.departmentPhoneNumber == "(831) 459-2969");
+    import utils.expect: expect;
 
-    assert("Richard Montgomery" in dept.faculty);
-    assert(dept.faculty["Richard Montgomery"].name == "Richard Montgomery");
-    assert(dept.faculty["Richard Montgomery"].title == "Professor");
-    assert(dept.faculty["Richard Montgomery"].department == "MATH");
-    assert(dept.faculty["Richard Montgomery"].description == 
+    auto dept = fetchDepartment("https://registrar.ucsc.edu/catalog/archive/17-18/programs-courses", "math");
+    expect(dept.departmentId).toEqual("MATH");
+    expect(dept.programUrl).toEqual("https://registrar.ucsc.edu/catalog/archive/17-18/programs-courses/program-statements/math.html");
+    expect(dept.coursesUrl).toEqual("https://registrar.ucsc.edu/catalog/archive/17-18/programs-courses/course-descriptions/math.html");
+    expect(dept.facultyUrl).toEqual("https://registrar.ucsc.edu/catalog/archive/17-18/programs-courses/faculty/math.html");
+ 
+    expect(dept.error).toEqual(null);
+    expect(dept.catalogVersion).toEqual("2017-18 General Catalog");
+    expect(dept.departmentName).toEqual("Mathematics");
+    expect(dept.departmentUrl).toEqual("http://www.math.ucsc.edu");
+    expect(dept.departmentAddress).toEqual("4111 McHenry");
+    expect(dept.departmentPhoneNumber).toEqual("(831) 459-2969");
+
+    expect(dept.faculty).toContain("Richard Montgomery");
+    expect(dept.faculty["Richard Montgomery"].name).toEqual("Richard Montgomery");
+    expect(dept.faculty["Richard Montgomery"].title).toEqual("Professor");
+    expect(dept.faculty["Richard Montgomery"].department).toEqual("MATH");
+    expect(dept.faculty["Richard Montgomery"].description).toEqual(
         "Celestial mechanics, differential geometry, gauge theory, mechanics (quantum and classical), and singularity theory");
 
-    assert("Marvin J. Greenberg" in dept.faculty);
-    assert(dept.faculty["Marvin J. Greenberg"].name == "Marvin J. Greenberg");
-    assert(dept.faculty["Marvin J. Greenberg"].title == "Emeriti");
-    assert(dept.faculty["Marvin J. Greenberg"].department == "MATH");
-    assert(dept.faculty["Marvin J. Greenberg"].description == "");
+    expect(dept.faculty).toContain("Richard Montgomery");
+    expect(dept.faculty["Marvin J. Greenberg"].name).toEqual("Marvin J. Greenberg");
+    expect(dept.faculty["Marvin J. Greenberg"].title).toEqual("Emeriti");
+    expect(dept.faculty["Marvin J. Greenberg"].department).toEqual("MATH");
+    expect(dept.faculty["Marvin J. Greenberg"].description).toEqual("");
 
-    assert("Daniele Venturi (Applied Math and Statistics)" !in dept.faculty);
-    assert("Daniele Venturi" !in dept.faculty);
+    expect(dept.faculty).toNotContain("Daniele Venturi (Applied Math and Statistics)");
+    expect(dept.faculty).toNotContain("Daniele Venturi");
 
-    assert("MATH 19B" in dept.courses);
-    assert(dept.courses["MATH 19B"].name == "MATH 19B");
-    assert(dept.courses["MATH 19B"].title == "Calculus for Science, Engineering, and Mathematics");
-    assert(dept.courses["MATH 19B"].division == "lower-division");
-    assert(dept.courses["MATH 19B"].terms == "FWS");
-    assert(dept.courses["MATH 19B"].instructor == "The Staff");
-    assert(dept.courses["MATH 19B"].description == 
+    expect(dept.courses).toContain("MATH 19B");
+    expect(dept.courses["MATH 19B"].name).toEqual("MATH 19B");
+    expect(dept.courses["MATH 19B"].title).toEqual("Calculus for Science, Engineering, and Mathematics");
+    expect(dept.courses["MATH 19B"].division).toEqual("lower-division");
+    expect(dept.courses["MATH 19B"].terms).toEqual("FWS");
+    expect(dept.courses["MATH 19B"].instructor).toEqual("The Staff");
+    expect(dept.courses["MATH 19B"].description).toEqual(
         "The definite integral and the fundamental theorem of calculus. Areas, volumes. Integration by parts, "~
         "trigonometric substitution, and partial fractions methods. Improper integrals. Sequences, series, "~
         "absolute convergence and convergence tests. Power series, Taylor and Maclaurin series. Students "~
@@ -61,13 +59,13 @@ unittest {
         "or Economics 11B. Prerequisite(s): course 19A or 20A or AP Calculus AB exam score of 4 or 5, or BC "~
         "exam score of 3 or higher, or IB Mathematics Higher Level exam score of 5 of higher");
 
-    assert("MATH 249B" in dept.courses);
-    assert(dept.courses["MATH 19B"].name == "MATH 249B");
-    assert(dept.courses["MATH 19B"].title == "Mechanics II");
-    assert(dept.courses["MATH 19B"].division == "graduate");
-    assert(dept.courses["MATH 19B"].terms == "*");
-    assert(dept.courses["MATH 19B"].instructor == "The Staff");
-    assert(dept.courses["MATH 19B"].description == 
+    expect(dept.courses).toContain("MATH 249B");
+    expect(dept.courses["MATH 19B"].name).toEqual("MATH 249B");
+    expect(dept.courses["MATH 19B"].title).toEqual("Mechanics II");
+    expect(dept.courses["MATH 19B"].division).toEqual("graduate");
+    expect(dept.courses["MATH 19B"].terms).toEqual("*");
+    expect(dept.courses["MATH 19B"].instructor).toEqual("The Staff");
+    expect(dept.courses["MATH 19B"].description).toEqual(
         "Covers symplectic geometry and classical Hamiltonian dynamics. Some of the key subjects are the Darboux "~
         "theorem, Poisson brackets, Hamiltonian and Langrangian systems, Legendre transformations, variational "~
         "principles, Hamilton-Jacobi theory, godesic equations, and an introduction to Poisson geometry. Courses "~
