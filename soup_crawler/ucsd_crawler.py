4c8e4e3dd8bc19e117f2d405550927865ccf5b44
Seiji Emery <seijiemery@gmail.com>
2018-07-16 22:55:23 -0700

    building out parse cases. not parsing semantically, just to see which courses are linked (roughly...)

--- soup_crawler/ucsd_crawler.py
+++ soup_crawler/ucsd_crawler.py
@@ -61,9 +61,80 @@ def get_page_courses (dept, item):
 
         # if not re.match(r'[A-Z][A-Za-z]+\s+\d+[A-Z]*', name):
         #     raise Exception("Invalid course name: '%s'", name)
-        print('\t'+name)
-        print('\t'+title)
-        print('\t'+descr)
+        # print('\t'+name)
+        # print('\t'+title)
+        # print('\t'+descr)
+        hits = descr.split("Prerequisites:")
+        prereqs = ". ".join(hits[1:]).strip().strip('.')
+        descr = hits[0]
+        # print(descr)
+
+        prereq_requirements = set()
+        def requirement (*reqs):
+            def sub (stuff):
+                for req in reqs:
+                    prereq_requirements.add(req)
+                return ''
+            return sub
+
+        def course_case_multiple_and (match):
+            print("AND case:")
+            print(match.group(1, 2, 3))
+
+        def course_case_single (match):
+            print("SINGLE CASE: '%s' '%s'"%match.group(1, 2))
+
+        def course_case_concatenative_or (match):
+            print("OR CONCATENATIVE CASE: '%s' '%s'"%match.group(1, 2))
+
+        def course_case_concatenative_and (match):
+            print("AND CONCATENATIVE CASE: '%s' '%s'"%match.group(1, 2))
+
+        def parse_annoying_edge_case (match):
+            dept, course, suffixes = match.group(1, 2, 3)
+            match = re.match(r'(\d+)([A-Z\-]+)', course)
+            enforce(match, "Course invalid - something broke...? dept = '%s', course = '%s', suffixes = '%s'",
+                dept, course, suffixes)
+            prefix, suffix = match.group(1, 2)
+            suffixes = suffixes.strip().split()
+            print("PARSED ANNOYING EDGE CASE: dept, prefix = '%s', '%s'; suffixes = '%s', %s"%(
+                dept, prefix, suffix, suffixes))
+
+        def parse_fucking_ridiculous_everything_case (match):
+            print("GOT RIDICULOUS CASE: '%s' '%s'"%(match.group(1), match.group(2)))
+
+        replace_cases = [
+            (r'none', ''),
+            (r'([A-Z][a-z]+(?:\s+[A-Z][a-z]+)*|[A-Z]+)\s+((\d+[A-Z\-]*)(\s+(and|or)\s+[A-Z])+)(?:\s+|$)', parse_annoying_edge_case),
+            # (r'([A-Z][a-z]+(?:\s+[A-Z][a-z]+)*|[A-Z]+)\s+(\d+[A-Z\-]*)', course_case_single),
+            # (r'([A-Z][a-z]+(?:\s+[A-Z][a-z]+)*|[A-Z]+)\s+(\d+[A-Z\-]*(?:\s+or\s+\d+[A-Z\-]*)*)', course_case_concatenative_or),
+            # (r'([A-Z][a-z]+(?:\s+[A-Z][a-z]+)*|[A-Z]+)\s+(\d+[A-Z\-]*(?:\s+and\s+\d+[A-Z\-]*)*)', course_case_concatenative_and),
+            (r'([A-Z][a-z]+(?:\s+[A-Z][a-z]+)*|[A-Z]+)\s+((?:\d+[A-Z\-]*(?:,\s+|,?\s+(?:or|and)\s+))*\d+[A-Z\-]*)', parse_fucking_ridiculous_everything_case),
+            # (r'([A-Z]\w+) ((\d\w+), )or (\d+\w+)', course_case_multiple_or),
+            
+            (r'(graduate standing( required)?|for graduate students|graduate student status)', requirement('GRADUATE_STANDING')),
+            (r'(Undergraduates must be seniors)', requirement('SENIOR_STANDING')),
+            (r'upper-division standing( required)?', requirement('UPPER_DIVISION_STANDING')),
+            (r'lower-division standing( required)?', requirement('LOWER_DIVISION_STANDING')),
+            (r'completion of college writing', requirement('COMPLETED_COLLEGE_WRITING')),
+            (r'admission to the MAS Clinical Research Program', requirement("ADMITTED_MAS_CLINICAL_RESEARCH_PROGRAM")),
+            (r'admission to (the )?MFA theatre program', requirement("ADMITTED_MFA_THEATRE_PROGRAM")),
+            (r'admission to PhD program in theatre', requirement("ADMITTED_PHD_THEATRE_PROGRAM")),
+            (r'Enrollment restricted to biological sciences graduate students', requirement("GRADUATE_STANDING", "BIOLOGICAL_SCIENCES_STUDENTS_ONLY")),
+            (r'limited to BMS graduate students except by consent of instructor', requirement("GRADUATE_STANDING", "BMS_STUDENTS_ONLY", "INSTRUCTOR_CONSENT")),
+            (r'second- or third-year design students only', requirement("SOPHOMORE_STANDING", "JUNIOR_STANDING", "DESIGN_STUDENTS_ONLY")),
+            (r'consent of instruct(or)?', requirement('INSTRUCTOR_CONSENT')),
+            (r'(consent of department|department approval( required)?)', requirement('DEPARTMENT_CONSENT')),
+            (r'grade of C\- or better in ', ''),
+            (r'\([FWS](,[FWS])*\)', ''),
+            # (r'^\s*((and|or|[,;\.|])\s*)+$', ''),
+        ]
+        if prereqs:
+            print(prereqs)
+            for r, s in replace_cases:
+                prereqs = re.sub(r, s, prereqs).strip()
+            # if prereqs:
+            print("\t'%s'"%prereqs)
 
     def process (soup):
         for a in soup.find_all('a'):
@@ -80,7 +151,7 @@ def get_page_courses (dept, item):
                     # descrip = '\n'.join(items[1:]).strip()
                 else:
                     header, descrip = text.strip(), ''
-                print(header)
+                # print(header)
                 if '.' in header:
                     items = header.split('.')
                     name = items[0].strip()
