ca29e8d39a296478e8401bf666866fffd5b1cb64
RaiderSoap <info@chivalrystudio.com>
2018-07-19 17:30:43 -0700

    Starting login page

---
+++ components/Login/LoginForm.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import TextField from 'material-ui/TextField';
+
+/**
+ *
+ * @param onSubmit
+ * @param onChange
+ * @param errors
+ * @param successMessage
+ * @param user
+ * @param toggleAuthenticateStatus
+ * @return {Element}
+ * @constructor
+ */
+const LoginForm = ({
+                     onSubmit,
+                     onChange,
+                     errors,
+                     successMessage,
+                     user,
+                     toggleAuthenticateStatus
+                   }) => (
+  <div>
+
+  </div>
+);
+
+
+export default LoginForm;
