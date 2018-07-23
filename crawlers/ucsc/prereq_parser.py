9db12d6b70fab403091135fe4c4ff0b9417128f0
Seiji Emery <seijiemery@gmail.com>
2018-07-23 16:04:29 -0700

    fixed index parsing (only was fetching half of all course listings...)

--- crawlers/ucsc/prereq_parser.py
+++ crawlers/ucsc/prereq_parser.py
@@ -1,14 +1,17 @@
 import re
 
 tokenizer = re.compile(r'''
-    (course|(?:[A-Z][a-z]+(?:\s+and)?)*\s+[A-Z][a-z]+) |
+    (by (?:consent|permission) of instructor) |
+    (satisfaction of the Entry Level Writing and Composition requirements) |
+    ([Bb]asic knowledge of computer programming languages is assumed) |
+    (courses?|(?:[A-Z][a-z]+(?:\s+and)?\s+)*[A-Z][a-z]+) |
     (\d+[A-Z]?) |
-    (;\s+and\s+) |
-    (;\s+or\s+) |
-    (;\s+) |
-    (,\s+and\s+) |
-    (,\s+or\s+) |
-    (,\s+) |
+    (;\s+and) |
+    (;\s+or) |
+    (;) |
+    (,\s+and) |
+    (,\s+or) |
+    (,) |
     (and) |
     (or) |
     (\s+) |
@@ -18,9 +21,11 @@ tokenizer = re.compile(r'''
 def parse_prereqs (prereqs, dept, depts):
     print("Parsing '%s'"%prereqs)
     depts['course'] = dept
+    depts['courses'] = dept
     course_prefix = "N/A "
     for match in re.finditer(tokenizer, prereqs):
-        (course, number, 
+        (instructor_consent, writing_req, programming_req,
+            course, number, 
             semi_and, semi_or, semi, 
             comma_and, comma_or, comma,
             _and, _or,
@@ -30,8 +35,10 @@ def parse_prereqs (prereqs, dept, depts):
         if error:
             with open ('unparsed', 'a') as f:
                 f.write(error+'\n')
+            # raise Exception("unmatched token(s) '%s' in '%s'"%(error, prereqs))
         elif course:
             course = course.strip()
             course_prefix = '%s '%depts[course].upper()
         elif number:
             print(course_prefix+number)
+
